76b89f1bccda001808ca5797a40ed5db53a2bf6c
bun2023 <138096357+bun2023@users.noreply.github.com>
2023-11-20 09:03:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,10 @@
 
 const swiper = new Swiper(".swiper", {
   effect: "fade",
-  speed: 2500, // スライドアニメーションのスピード（ミリ秒）
+  speed: 5000, // スライドアニメーションのスピード（ミリ秒）
   loop: true,
-  loopAdditionalSlides: 1,
   autoplay: { // 自動再生させる
     // display: 3000,
-    disableOnInteraction: false, // ユーザーが操作しても自動再生を止めない
-    waitForTransition: false, // アニメーションの間も自動再生を止めない（最初のスライドの表示時間を揃えたいときに）
   },
   // navigation: {
   //   nextEl: ".swiper-button-next",
